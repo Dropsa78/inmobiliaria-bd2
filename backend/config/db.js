541371b02ea1c5c1b36e45007334f42087768415
Dropsa78 <dropsa78@gmail.com>
2026-04-13 19:45:46 -0600

    fix: export pool nombrado

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,16 +9,14 @@
   //database: "Inmobiliaria-db",
   //password: "Gu7hs2ki1",
   //port: 5432
-//});
+//});git 
 
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
-export default pool;
