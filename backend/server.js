10097d8bfb9e976705f8bdce11840ede475007c9
Dropsa78 <dropsa78@gmail.com>
2026-04-13 19:52:14 -0600

    fix: cors y port dinamico

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 import express from "express";
 import propiedadesRoutes from "./routes/propiedades.js";
-import authRoutes from "./routes/authRoutes.js";  // ← corregido
-import { pool } from "./config/db.js";
-import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 import favoritosRoutes from "./routes/favoritosRoutes.js";
-
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://inmobiliaria-app-c64be.web.app"
+}));
 app.use(express.json());
 app.use(express.static("public"));
+
 app.use("/favoritos", favoritosRoutes);
 app.use("/propiedades", propiedadesRoutes);
-app.use("/auth", authRoutes);               // → /auth/cliente/login ✅
-                                            // → /auth/cliente/registro ✅
-                                            // → /auth/agente/login ✅
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
